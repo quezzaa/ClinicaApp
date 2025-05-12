19cd85453e2cb0e29a0e217f78cdc8c3207ab702
quezzaa <quezadita0409@gmail.com>
2025-05-11 20:36:44 -0600

    FirstCommit

---
+++ src/app/components/citas/citas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-citas',
+  standalone: true,
+  imports: [],
+  templateUrl: './citas.component.html',
+  styleUrl: './citas.component.css'
+})
+export class CitasComponent {
+
+}
---
+++ src/app/components/tratamientos/tratamientos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tratamientos',
+  standalone: true,
+  imports: [],
+  templateUrl: './tratamientos.component.html',
+  styleUrl: './tratamientos.component.css'
+})
+export class TratamientosComponent {
+
+}
